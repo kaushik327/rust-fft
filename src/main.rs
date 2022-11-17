40b57af55f367072e4d98e296b7754e19e852c9b
Kaushik R Varadharajan <kv22@illinois.edu>
2022-11-17 08:13:01 +0000

    graphing audio file with plotly for some reason

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use std::f32::consts::PI;
 use std::i16;
 use hound;
+use plotly::{Plot, Scatter};
 
-pub fn read_wav(filename: &str) {
+pub fn read_wav(filename: &str) -> Vec<i16> {
     let mut reader = hound::WavReader::open(filename).unwrap();
-    
     let samples: Vec<i16> = reader.samples::<i16>().map(|x| x.unwrap()).collect();
-    
-    println!("{:?}", samples)
+    // println!("{:?}", samples);
     return samples;
+}
+
+pub fn display_samples(samples: Vec<i16>) {
+    let mut plot = Plot::new();
+    let trace = Scatter::new((0..samples.len()).collect(), samples);
+    plot.add_trace(trace);
+    plot.write_html("output/out.html");
+    // Creates html file in output folder containing the graph.
+    // Definitely switching to a different crate in the future.
 }
 
 
 fn main() {
     println!("Hello, world!");
-    read_wav("/home/vagrant/src/rust-fft/audio/clap.wav");
+    let samples = read_wav("/home/vagrant/src/rust-fft/audio/clap.wav");
+    display_samples(samples);
 }
