572290dc0ba2502359f04892d830d5a8e6a306b9
Kaushik R Varadharajan <kv22@illinois.edu>
2022-11-17 18:43:31 +0000

    graph labels

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::i16;
 use hound;
-use plotly::{Plot, Scatter};
+use plotly::{
+    Plot, Scatter,
+    layout::{Axis, Layout},
+    common::{Title},
+};
 
 pub fn read_wav(filename: &str) -> Vec<i16> {
     let mut reader = hound::WavReader::open(filename).unwrap();
@@ -11,8 +15,14 @@ pub fn read_wav(filename: &str) -> Vec<i16> {
 
 pub fn display_samples(samples: Vec<i16>) {
     let mut plot = Plot::new();
-    let trace = Scatter::new((0..samples.len()).collect(), samples);
+    let time_scale = (0..samples.len()).map(|x| x as f32 / 44100.0).collect();
+    let trace = Scatter::new(time_scale, samples);
     plot.add_trace(trace);
+    let layout = Layout::new()
+        .title(Title::new("Audio"))
+        .x_axis(Axis::new().title(Title::new("Time (s)")))
+        .y_axis(Axis::new().title(Title::new("Amplitude (i16)")));
+    plot.set_layout(layout);
     plot.write_html("output/out.html");
     // Creates html file in output folder containing the graph.
     // Definitely switching to a different crate in the future.
@@ -20,7 +30,6 @@ pub fn display_samples(samples: Vec<i16>) {
 
 
 fn main() {
-    println!("Hello, world!");
     let samples = read_wav("/home/vagrant/src/rust-fft/audio/clap.wav");
     display_samples(samples);
 }
