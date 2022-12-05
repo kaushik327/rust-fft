31126d93af7a1f8afe717b2014f973f2f256091b
Kaushik R Varadharajan <kv22@illinois.edu>
2022-12-05 03:06:49 +0000

    graph edits

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,62 @@
 use std::i16;
 use hound;
-use plotly::{
-    Plot, Scatter,
-    layout::{Axis, Layout},
-    common::{Title},
-};
 use num_complex::Complex;
-use std::f64::consts::PI;
+use std::f32::consts::PI;
+use charts::{Chart, ScaleLinear, MarkerType, PointLabelPosition, LineSeriesView};
+
 
 pub fn read_wav(filename: &str) -> Vec<i16> {
     let mut reader = hound::WavReader::open(filename).unwrap();
     let samples: Vec<i16> = reader.samples::<i16>().map(|x| x.unwrap()).collect();
-    // println!("{:?}", samples);
     return samples;
 }
 
 pub fn display_samples(samples: Vec<i16>) {
-    let mut plot = Plot::new();
-    let time_scale = (0..samples.len()).map(|x| x as f32 / 44100.0).collect();
-    let trace = Scatter::new(time_scale, samples);
-    plot.add_trace(trace);
-    let layout = Layout::new()
-        .title(Title::new("Audio"))
-        .x_axis(Axis::new().title(Title::new("Time (s)")))
-        .y_axis(Axis::new().title(Title::new("Amplitude (i16)")));
-    plot.set_layout(layout);
-    plot.write_html("output/out.html");
-    // Creates html file in output folder containing the graph.
-    // Definitely switching to a different crate in the future.
+    let width = 800;
+    let height = 600;
+    let (top, right, bottom, left) = (90, 40, 50, 60);
+
+    let x = ScaleLinear::new()
+        .set_domain(vec![0.0, samples.len() as f32])
+        .set_range(vec![0, width - left - right]);
+
+    let y = ScaleLinear::new()
+        .set_domain(vec![i16::MIN as f32, i16::MAX as f32])
+        .set_range(vec![height - top - bottom, 0]);
+
+    let line_data = samples.iter()
+                              .enumerate()
+                              .map(|(x, y)| (x as f32, *y as f32))
+                              .collect();
+
+    // Create Line series view that is going to represent the data.
+    let line_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_marker_type(MarkerType::Circle)
+        .set_label_position(PointLabelPosition::N)
+        .load_data(&line_data).unwrap();
+
+    // Generate and save the chart.
+    Chart::new()
+        .set_width(width)
+        .set_height(height)
+        .set_margins(top, right, bottom, left)
+        .add_title(String::from("Audio"))
+        .add_view(&line_view)
+        .add_axis_bottom(&x)
+        .add_axis_left(&y)
+        .add_left_axis_label("Time")
+        .add_bottom_axis_label("Amplitude")
+        .save("/home/vagrant/src/rust-fft/output/scatter-chart.svg").unwrap();
 }
 
-pub fn fft(x: Vec<i16>) -> Vec<Complex<f64>> {
+pub fn fft(x: Vec<i16>) -> Vec<Complex<f32>> {
     // radix-2 Cooley-Tukey FFT; recursive algorithm.
     // Assumes x.len() is a power of 2.
     let length = x.len();
     if length == 1 {
-        return vec![Complex::new(x[0] as f64, 0.0)];
+        return vec![Complex::new(x[0] as f32, 0.0)];
     }
     let mut x_even = Vec::new();
     let mut x_odd = Vec::new();
@@ -48,23 +69,25 @@ pub fn fft(x: Vec<i16>) -> Vec<Complex<f64>> {
     let mut fft = Vec::new();
     fft.push(fft_even[0] + fft_odd[0]);
     for k in 1..length/2 {
-        fft.push(fft_even[k] + fft_odd[k] * Complex::new(0.0, -2.0*PI*(k as f64)/(length as f64)).exp());
+        fft.push(fft_even[k] + fft_odd[k] * Complex::new(0.0, -2.0*PI*(k as f32)/(length as f32)).exp());
     }
     fft.push(fft_even[0] - fft_odd[0]);
     for k in (1..length/2).rev() {
-        fft.push(fft[k].conj())
+        fft.push(fft[k].conj());
     }
     return fft;
 }
 
 
 fn main() {
-    // let samples = read_wav("/home/vagrant/src/rust-fft/audio/clap.wav");
-    // display_samples(samples);
+    let samples = read_wav("/home/vagrant/src/rust-fft/audio/clap.wav");
 
-    // https://scistatcalc.blogspot.com/2013/12/fft-calculator.html
-    let fft = fft(vec![1, 6, 2, 7, 3, 4, 6, 1, 2, 7, 4, 9, 1, 8, 4, 2]);
-    let formatted: Vec<Vec<f64>> = fft.iter().map(|x| vec![x.re, x.im]).collect();
-    println!("{:?}", formatted);
+    display_samples(samples);
+
+    // let fft = fft(vec![1, 6, 2, 7, 3, 4, 6, 1, 2, 7, 4, 9, 1, 8, 4, 2]);
+    // let formatted: Vec<Vec<f32>> = fft.iter().map(|x| vec![x.re, x.im]).collect();
+    // println!("{:?}", formatted);
+    
     // Prints the vector of complex numbers like [real, imaginary]
+    // https://scistatcalc.blogspot.com/2013/12/fft-calculator.html
 }
